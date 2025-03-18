f974dd66767e93bb44fadcb7bb7ce66ba2a9bd8c
Yohan-ParkGist <68223714+Yohan-ParkGist@users.noreply.github.com>
2025-03-18 10:10:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,15 @@ import pandas as pd
 # YOU MUST WRITE YOUR STUDENT ID IN THE VARIABLE STUDENT_ID
 # EXAMPLE: STUDENT_ID = "12345678"
 #####################
-STUDENT_ID = "12345678"
+STUDENT_ID = "20251189"
 
 def load_data():
-    # EXAMPLE
-    # Load the data from a csv file
-    # You can change function
-    train = pd.read_csv('./data/redwine_train.csv')
-    test = pd.read_csv('./data/redwine_test.csv')
+    train = pd.read_csv('./dataset/simple_seq.train.csv')
+    test = pd.read_csv('./dataset/simple_seq.test.csv')
 
     return train, test
 
 def save_data(df1, df2):
-    # EXAMPLE
-    # Save the data to a csv file
-    # You can change function
     # BUT you should keep the file name as "{STUDENT_ID}_simple_seq.p#.answer.csv"
     df1.to_csv(f'{STUDENT_ID}_simple_seq.p1.answer.csv')
     df2.to_csv(f'{STUDENT_ID}_simple_seq.p2.answer.csv')
