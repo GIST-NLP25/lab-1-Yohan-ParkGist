540e1fd4e32685953ff86b0b0debe8aa60d2a382
Yohan-ParkGist <68223714+Yohan-ParkGist@users.noreply.github.com>
2025-03-19 18:07:56 +0900

    test file made

--- main.py
+++ main.py
@@ -6,16 +6,11 @@ import pandas as pd
 #####################
 STUDENT_ID = "20251189"
 
-def load_data():
-    train = pd.read_csv('./dataset/simple_seq.train.csv')
-    test = pd.read_csv('./dataset/simple_seq.test.csv')
-
-    return train, test
-
 def save_data(df1, df2):
     # BUT you should keep the file name as "{STUDENT_ID}_simple_seq.p#.answer.csv"
     df1.to_csv(f'{STUDENT_ID}_simple_seq.p1.answer.csv')
     df2.to_csv(f'{STUDENT_ID}_simple_seq.p2.answer.csv')
+
 
 def main():
     # WRITE YOUR CODE HERE
